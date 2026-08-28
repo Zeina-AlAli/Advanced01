2dd2cb51d5f696c34ea517ad013f62ea414d7617
Zeina-AlAli <zeinamahmoud995@gmail.com>
2026-08-29 00:46:28 +0300

    Question01

--- Advanced01/Program.cs
+++ Advanced01/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Q1 : Generic Class
+            //Console.WriteLine("- Generic class is a class that allows us to write with type parameters where the actual type is specified when the is used.");
+            //Console.WriteLine(("- We use Generics because it provides Type safety, code reusability, better performance and cleaner code");
+            #endregion
         }
     }
 }
