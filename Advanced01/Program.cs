f57722bf35dd55d49030535b9e47c408be1fa42f
Zeina-AlAli <zeinamahmoud995@gmail.com>
2026-08-29 00:54:40 +0300

    Question02

--- Advanced01/Program.cs
+++ Advanced01/Program.cs
@@ -8,6 +8,13 @@
             //Console.WriteLine("- Generic class is a class that allows us to write with type parameters where the actual type is specified when the is used.");
             //Console.WriteLine(("- We use Generics because it provides Type safety, code reusability, better performance and cleaner code");
             #endregion
+
+            #region Q2 : Use Generic class
+            //Container<int> container = new Container<int>();
+            //container.Add(1);
+            //int GetNum = container.Get();
+            //Console.WriteLine(GetNum);
+            #endregion
         }
     }
 }
